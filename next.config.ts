00984e3498c648e0fca9891d5fc859fbf4b02665
duchmx <duchlobo@gmail.com>
2026-04-24 13:30:49 -0500

    feat: configure remote patterns for Supabase  image hosting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '54321',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
